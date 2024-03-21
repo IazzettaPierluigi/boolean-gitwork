90ec11a5cb7ac8aacd0b510744fe9b7cce9f0b18
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-03-21 18:17:31 +0100

    aef

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -6,5 +6,5 @@
 
 @section('content')
     <h1>welcome page3qerrwfgrwgrhyrre</h1>
-     
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod aut blanditiis est nesciunt, porro, eos adipisci esse cupiditate expedita vitae sed, voluptates corrupti veniam odit magnam maiores sunt beatae! Vitae.</p>
 @endsection
